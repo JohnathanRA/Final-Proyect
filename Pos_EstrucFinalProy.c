5a2d68af1b75e31f66a27811d09d3edba4c6e978
JohnathanRA <31198069+JohnathanRA@users.noreply.github.com>
2017-11-17 11:57:31 -0600

    Create Pos_EstrucFinalProy.c
    Una posible estructura de la estructura en la que nos podemos basar para realizar el final.

---
+++ Pos_EstrucFinalProy.c
@@ -0,0 +1,13 @@
+struct Reactivo{
+	
+	char pregunta[100];
+	char op1[100];
+	char op2[100];
+	char op3[100];
+	char op4[100];
+	int resp;
+	int presento=0;  //1 si se presento, 0 No se presento
+};
+
+struct Reactivo preguntas[30];
+struct Reactivo examen[10];
