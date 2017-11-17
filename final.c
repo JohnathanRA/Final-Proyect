c29eb40dd3947349ba37180f6e894477bab15890
JohnathanRA <31198069+JohnathanRA@users.noreply.github.com>
2017-11-17 13:23:47 -0600

    Update final.c

--- final.c
+++ final.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 
-  main()
-  {FILE *archivo;
-   archivo=fopen("hola.txt", "r");
-   if (archivo==NULL){
-     printf("No existe el archivo");
-     return 0;
+int main(){
+  FILE *archivo;
+  char Usu_Alu[20];
+  int i, j, Cue_Alu;
+  
+  printf("Debebs ingresar tu usuario y tu numero de cuenta.");
+  printf("\n\nIngresa un usuario de max. 20 caracteres:");
+  gets(Usu_Alu);
+  
+  printf("Ingresa tu numero de cuenta:");
+  scanf("%d", & Cue_Alu);
+  
+  printf("%s, %d", Usu_Alu, Cue_Alu);
+  
+  archivo=fopen("hola.txt", "r");
+  if (archivo==NULL){
+   printf("\nNo existe el archivo");
   }
+  
+  return 0;
+}
