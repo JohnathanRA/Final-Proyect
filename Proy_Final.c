19fb23015876a996b81b4f26ece5c6d4b5906700
JyuHysteria <31145605+JyuHysteria@users.noreply.github.com>
2017-12-08 13:22:33 -0600

    Update Proy_Final.c

--- Proy_Final.c
+++ Proy_Final.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
 
+void datos();
+
 int main(){
-  FILE *archivo;
-  char Usu_Alu[20];
-  int i, j, Cue_Alu;
-  
-  printf("Debebs ingresar tu usuario y tu numero de cuenta.");
-  printf("\n\nIngresa un usuario de max. 20 caracteres:");
-  gets(Usu_Alu);
-  
-  printf("Ingresa tu numero de cuenta:");
-  scanf("%d", & Cue_Alu);
-  
-  printf("%s, %d", Usu_Alu, Cue_Alu);
-  
-  archivo=fopen("hola.txt", "r");
-  if (archivo==NULL){
-   printf("\nNo existe el archivo");
-  }
-  
-  return 0;
+	datos();
+	
+	
+	
+	
+  	return 0;
 }
+
+void datos(){
+	FILE *archivo;
+  	int CueAlu, nuevo;
+  
+ 	printf("Ingresa tu numero de cuenta:");
+  	scanf("%d", & CueAlu);
+  	
+  	archivo=fopen("Info.txt", "a");
+  	
+  	while(!feof(archivo)){
+		fscanf(archivo, ",%d", &nuevo);
+  	}
+  	
+  	fprintf(archivo, ",%d", CueAlu);
+  	
+	fclose(archivo);
+}
+
