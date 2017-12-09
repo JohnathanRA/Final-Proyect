14e85868b75f73e57133a5fc5608c71ee1733706
JyuHysteria <31145605+JyuHysteria@users.noreply.github.com>
2017-12-08 23:52:15 -0600

    Update Proy_Final.c

--- Proy_Final.c
+++ Proy_Final.c
@@ -1,31 +1,46 @@
 #include<stdio.h>
 
-void datos();
+int num_cue(int *cue_alu);
 
-int main(){
-	datos();
+int main(){	
+	FILE *archivo;
+	char usu[20];
+	int cue, t;
+	int *a, *b;
+	b=&cue;
 	
+	printf("Ingresa tu numero de cuenta y tu nombre \nIngresa tu numero de cuenta:");
+	scanf("%d",&*b);
+	t=num_cue(b);
 	
-	
-	
-  	return 0;
+	if(t==1){
+		printf("Ingresa tu nombre:");
+		gets("")
+	}
 }
 
-void datos(){
+int num_cue(int *cue_alu){
 	FILE *archivo;
-  	int CueAlu, nuevo;
-  
- 	printf("Ingresa tu numero de cuenta:");
-  	scanf("%d", & CueAlu);
-  	
-  	archivo=fopen("Info.txt", "a");
-  	
-  	while(!feof(archivo)){
-		fscanf(archivo, ",%d", &nuevo);
-  	}
-  	
-  	fprintf(archivo, ",%d", CueAlu);
-  	
+	int n, y=0; 
+	
+	archivo=fopen("info.txt", "r");
+	
+	while(!feof(archivo)){
+		fscanf(archivo,"%d,%d ",&n,&y);
+		if(n==*cue_alu){
+			printf("Ya realizaste el examen, tu calificacion registrada es: %d\n", y);
+			n=-1;
+			break;
+		}
+		n=0;
+	}
 	fclose(archivo);
+	
+	if(n!=-1){
+		archivo=fopen("info.txt", "a");
+		fprintf(archivo,"%d,%d ",*cue_alu,y);
+		fclose(archivo);
+		return 1;
+	}
 }
 
